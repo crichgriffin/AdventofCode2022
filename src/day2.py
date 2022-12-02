7488e57891d57dd06137d92dfc087280169911b1
charlotterich <25774602+charlotterich@users.noreply.github.com>
2022-12-02 16:56:34 +0000

    day2

---
+++ src/day2.py
@@ -0,0 +1,59 @@
+# Day 2 - Rock paper scissors
+
+import pandas as pd
+
+# part1
+
+def rps_score(val1, val2):
+    input_map=[("A", "X"), ("B", "Y"), ("C", "Z")]
+    val2 = [x for x, y in input_map if y == val2][0]
+    init_score = [("A", 1), ("B", 2), ("C", 3)]
+    wins = [("A", "B"), ("B", "C"), ("C", "A")]
+    init_score=[y for x, y in init_score if x == val2][0]
+    if val1 == val2:
+        init_score += 3
+    else:
+        win_match = [y for x, y in wins if x==val1][0]
+        if val2 == win_match:
+            init_score +=6
+        else:
+            init_score += 0
+    return init_score
+
+file1 = pd.read_csv('data/day2.txt', sep=' ', header=None)
+
+all_scores = file1.apply(lambda x: rps_score(*x), axis=1)
+sum(all_scores)
+
+
+def action_to_take(val1, val2):
+    # X = lose
+    losses = [("A", "Z"), ("B", "X"), ("C", "Y")]
+    # Y = draw
+    input_map=[("A", "X"), ("B", "Y"), ("C", "Z")]
+    # Z = win
+    wins = [("A", "Y"), ("B", "Z"), ("C", "X")]
+    if val2 == "X":
+        choice = [y for x, y in losses if x==val1][0]
+    elif val2 == "Z":
+        choice = [y for x, y in wins if x==val1][0]
+    else:
+        choice = [y for x, y in input_map if x==val1][0]
+    return choice
+
+   
+def part2(val1, val2):   
+    return rps_score(val1, action_to_take(val1, val2))
+
+    
+part2_scores = file1.apply(lambda x: part2(*x), axis=1)
+sum(part2_scores)
+
+part2("A", "Y") ==4
+part2("B", "X") == 1
+part2("C", "Z") == 7
+
+
+all_scores = file1.apply(lambda x: part2(*x), axis=1)
+
+sum(list(all_scores))
